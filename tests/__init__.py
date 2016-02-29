7fe12da6bc79b8eebf45cf09f7e4b15591ca85a5
Matěj Cepl <mcepl@cepl.eu>
2016-02-29 13:49:58 +0100

    Setup logging.

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,3 +1,5 @@
+import logging
+
 from platform import linux_distribution
 
 distro_string = linux_distribution(supported_dists=('redhat', 'fedora',
@@ -5,3 +7,5 @@ distro_string = linux_distribution(supported_dists=('redhat', 'fedora',
                                    full_distribution_name=False)[0]
 plat_fedora = distro_string in ['redhat', 'fedora']
 plat_debian = distro_string in ['debian']
+logging.basicConfig(format='%(levelname)s:%(funcName)s:%(message)s',
+                    level=logging.DEBUG)
