09f9d51cecdcc7d83c42be43a3b38476f559ecf0
Matěj Cepl <mcepl@cepl.eu>
2016-05-27 01:27:23 +0200

    M2Crypto/Rand.py: Add PEP-484 type hints in comments.

--- M2Crypto/Rand.py
+++ M2Crypto/Rand.py
@@ -9,11 +9,15 @@ from M2Crypto import m2
 __all__ = ['rand_seed', 'rand_add', 'load_file', 'save_file', 'rand_bytes',
            'rand_pseudo_bytes', 'rand_file_name', 'rand_status']
 
-rand_seed = m2.rand_seed
-rand_add = m2.rand_add
-load_file = m2.rand_load_file
-save_file = m2.rand_save_file
-rand_bytes = m2.rand_bytes
-rand_pseudo_bytes = m2.rand_pseudo_bytes
-rand_file_name = m2.rand_file_name
-rand_status = m2.rand_status
+rand_seed = m2.rand_seed  # type: (bytes) -> None
+rand_add = m2.rand_add  # type: (bytes, float) -> None
+load_file = m2.rand_load_file  # type: (bytes, int) -> int
+save_file = m2.rand_save_file  # type: (bytes) -> int
+rand_bytes = m2.rand_bytes  # type: (int) -> bytes
+rand_pseudo_bytes = m2.rand_pseudo_bytes  # type: (int) -> tuple[bytes, int]
+rand_status = m2.rand_status  # type: () -> int
+
+
+def rand_file_name():
+    # type: () -> bytes
+    return m2.rand_file_name()
