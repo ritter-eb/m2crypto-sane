eef28911dd545649fd180047f53042fe0af35706
JimC <jim@carroll.com>
2016-07-29 11:12:33 -0400

    Removed swig installation as per discussion w/ mcpel

--- add-swig-to-centos.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-# Script to build swig on centos6.
-cd /tmp && \
-    git clone https://github.com/swig/swig.git && \
-    cd swig && \
-    ./autogen.sh && \
-    ./configure && \
-    make  && make install
