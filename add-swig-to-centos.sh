043e64d0a532b55b2ead4a92f94f607aa7d8ca2f
JimC <jim@carroll.com>
2016-07-28 22:54:31 -0400

    Modified script to improve swig build

--- add-swig-to-centos.sh
+++ add-swig-to-centos.sh
@@ -1,6 +1,5 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 # Script to build swig on centos6.
-# note; 'yum install swig' delivers swig 1.0. Cannot use.
 git clone https://github.com/swig/swig.git && \
     cd swig && \
     ./autogen.sh && \
