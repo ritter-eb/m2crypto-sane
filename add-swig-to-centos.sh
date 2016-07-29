d1d0d75170162e94c75763ca3e44cd3e259f8797
JimC <jim@carroll.com>
2016-07-28 23:02:25 -0400

    typo

--- add-swig-to-centos.sh
+++ add-swig-to-centos.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env sh
 # Script to build swig on centos6.
-git clone https://github.com/swig/swig.git && \
+cd /tmp && \
+    git clone https://github.com/swig/swig.git && \
     cd swig && \
     ./autogen.sh && \
     ./configure && \
-    ./make install
+    make install
